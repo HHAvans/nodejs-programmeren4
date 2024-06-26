cbab969f4df851283ef1b0b8a02e38554a9b15b9
Homerus Hoogland <hj.hoogland@student.avans.nl>
2024-06-26 13:16:51 +0200

    Input validation update

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -19,6 +19,9 @@ const notFound = (req, res, next) => {
 const validateUserCreate = (req, res, next) => {
     try {
         assert(req.body.firstName, 'Missing or incorrect firstName field')
+        assert(req.body.lastName, 'Missing or incorrect lastName field')
+        assert(req.body.email, 'Missing or incorrect email field')
+        assert(req.body.password, 'Missing or incorrect password field')
         chai.expect(req.body.firstName).to.not.be.empty
         chai.expect(req.body.firstName).to.be.a('string')
         chai.expect(req.body.firstName).to.match(
@@ -37,6 +40,60 @@ const validateUserCreate = (req, res, next) => {
     }
 }
 
+const validateUserEdit = (req, res, next) => {
+    try {
+        assert(req.body.firstName, 'Missing or incorrect firstName field')
+        assert(req.body.lastName, 'Missing or incorrect lastName field')
+        assert(req.body.email, 'Missing or incorrect email field')
+        assert(req.body.password, 'Missing or incorrect password field')
+        chai.expect(req.body.firstName).to.not.be.empty
+        chai.expect(req.body.firstName).to.be.a('string')
+        chai.expect(req.body.firstName).to.match(
+            /^[a-zA-Z]+$/,
+            'firstName must be a string'
+        )
+        logger.trace('User successfully validated')
+        next()
+    } catch (ex) {
+        logger.trace('User validation failed:', ex.message)
+        next({
+            status: 400,
+            message: ex.message,
+            data: {}
+        })
+    }
+}
+
+const validateInput = (req, res, next) => {
+
+    const beforeAt = (req.body.email).split('@')[0]
+    const afterAt = (req.body.email).split('@')[1]
+    // Validate email format
+    if (!(req.body.email).includes("@") || beforeAt.split('.')[0].length > 1 || beforeAt.split('.')[1].length < 2 || afterAt.split('.')[0].length < 2 || (afterAt.split('.')[1].length < 2 || afterAt.split('.')[1].length > 3)) {
+        const error = new Error('Invalid email address');
+        logger.error(error);
+        next({
+            status: 400,
+            message: error.message,
+            data: {}
+        })
+        return;
+    }
+
+    // Validate password length
+    if (req.body.password.length < 8) {
+        const error = new Error('Password must be at least 8 characters long');
+        logger.error(error);
+        next({
+            status: 400,
+            message: error.message,
+            data: {}
+        })
+        return
+    }
+    next()
+}
+
 const validateAuthorization = (req, res, next) => {
     //Check if cookid matches authorization
     if (req.params.id != req.userId) {
@@ -50,11 +107,11 @@ const validateAuthorization = (req, res, next) => {
 }
 
 // Userroutes
-router.post('/api/user', validateUserCreate, userController.create) // Create user
+router.post('/api/user', validateUserCreate, validateInput, userController.create) // Create user
 router.get('/api/user', userController.getAll) // Get all users
 router.get('/api/user/profile', validateToken, userController.getProfile) // Get profile of currently logged in user
 router.get('/api/user/:userId', userController.getById) // Get user with user id
-router.put('/api/user/:id', validateToken, validateAuthorization, validateUserCreate, userController.edit) // Put new user in existing id
+router.put('/api/user/:id', validateToken, validateAuthorization, validateUserEdit, validateInput, userController.edit) // Put new user in existing id
 router.delete('/api/user/:id', validateToken, validateAuthorization, userController.delete) // Delete user by id
 
 module.exports = router
